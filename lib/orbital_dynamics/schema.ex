b12eb661e48ea1ef41fe62b70973174bdd27ee1a
Donovan Rost <donovan.rost@gmail.com>
2026-07-17 07:54:21 -0600

    collapse deferred priority callback delegate

--- lib/orbital_dynamics/schema.ex
+++ lib/orbital_dynamics/schema.ex
@@ -5845,7 +5845,7 @@ defmodule OrbitalDynamics.Schema do
       validate_optional_actual_data_rate_throughput_derivation:
         &validate_optional_actual_data_rate_throughput_derivation/4,
       validate_contact_contention_deferred_priority:
-        &validate_contact_contention_deferred_priority/3,
+        &OrbitalDynamics.Schema.ContactContentionReportContracts.validate_deferred_priority/3,
       validate_priority_field_evidence_counts: &validate_priority_field_evidence_counts/3,
       validate_override_count_matches_ids: &validate_override_count_matches_ids/5,
       validate_station_calendar_contact_counts: &validate_station_calendar_contact_counts/3
@@ -6112,14 +6112,6 @@ defmodule OrbitalDynamics.Schema do
 
   defp validate_optional_contact_contention_resolution_report(issues, _report),
     do: [error("$.contact_contention_resolution_report", "must be an object") | issues]
-
-  defp validate_contact_contention_deferred_priority(issues, path, row) do
-    OrbitalDynamics.Schema.ContactContentionReportContracts.validate_deferred_priority(
-      issues,
-      path,
-      row
-    )
-  end
 
   defp validate_priority_field_evidence_counts(issues, path, counts) when is_map(counts) do
     Enum.reduce(counts, issues, fn {field, count}, acc ->
@@ -7823,7 +7815,7 @@ defmodule OrbitalDynamics.Schema do
         &validate_optional_actual_data_rate_throughput_derivation/4,
       validate_optional_lifecycle_transition: &validate_optional_lifecycle_transition/4,
       validate_contact_contention_deferred_priority:
-        &validate_contact_contention_deferred_priority/3,
+        &OrbitalDynamics.Schema.ContactContentionReportContracts.validate_deferred_priority/3,
       validate_priority_field_evidence_counts: &validate_priority_field_evidence_counts/3,
       validate_optional_branch_comparison_source_row:
         &validate_optional_branch_comparison_source_row/3,
