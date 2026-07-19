935abd1b83cf7185697b6661a55d280345f0ffb9
Donovan Rost <donovan.rost@gmail.com>
2026-07-19 17:25:45 -0600

    extract contact contention station calendar context

--- lib/orbital_dynamics/communications/contact_contention.ex
+++ lib/orbital_dynamics/communications/contact_contention.ex
@@ -16,54 +16,6 @@ defmodule OrbitalDynamics.Communications.ContactContention do
   @contact_directions ~w(downlink uplink command tracking health_check)
   @command_contact_directions ~w(command uplink)
   @health_check_contact_directions ~w(health_check)
-  @unavailable_aliases ["outage", "down", "offline"]
-  @station_availability_severity %{
-    "unavailable" => 5,
-    "maintenance" => 5,
-    "reserved" => 4,
-    "reduced_capacity" => 3,
-    "available" => 1
-  }
-  @station_capacity_fraction_paths [
-    ["capacity_pack_capacity_fraction"],
-    ["station_capacity_fraction"],
-    ["capacity_fraction"],
-    ["throughput_model", "station_capacity_fraction"],
-    ["throughput_model", "capacity_fraction"],
-    ["capacity_model", "station_capacity_fraction"],
-    ["capacity_model", "capacity_fraction"],
-    ["activity_context", "station_capacity_fraction"],
-    ["activity_context", "capacity_fraction"]
-  ]
-  @station_capacity_percent_paths [
-    ["station_capacity_percent"],
-    ["capacity_percent"],
-    ["throughput_model", "station_capacity_percent"],
-    ["throughput_model", "capacity_percent"],
-    ["capacity_model", "station_capacity_percent"],
-    ["capacity_model", "capacity_percent"],
-    ["activity_context", "station_capacity_percent"],
-    ["activity_context", "capacity_percent"]
-  ]
-  @station_capacity_value_paths [
-    {:fraction, ["capacity_pack_capacity_fraction"]},
-    {:fraction, ["station_capacity_fraction"]},
-    {:fraction, ["capacity_fraction"]},
-    {:percent, ["station_capacity_percent"]},
-    {:percent, ["capacity_percent"]},
-    {:fraction, ["throughput_model", "station_capacity_fraction"]},
-    {:fraction, ["throughput_model", "capacity_fraction"]},
-    {:percent, ["throughput_model", "station_capacity_percent"]},
-    {:percent, ["throughput_model", "capacity_percent"]},
-    {:fraction, ["capacity_model", "station_capacity_fraction"]},
-    {:fraction, ["capacity_model", "capacity_fraction"]},
-    {:percent, ["capacity_model", "station_capacity_percent"]},
-    {:percent, ["capacity_model", "capacity_percent"]},
-    {:fraction, ["activity_context", "station_capacity_fraction"]},
-    {:fraction, ["activity_context", "capacity_fraction"]},
-    {:percent, ["activity_context", "station_capacity_percent"]},
-    {:percent, ["activity_context", "capacity_percent"]}
-  ]
   @station_reservation_priority_match_statuses ~w(matched owned owner_matched)
   @station_reservation_priority_statuses ~w(approved confirmed reserved held)
   @resolution_selection_rules ~w(
@@ -119,6 +71,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
     FeedbackContext,
     PriorityOverrides,
     ResolutionSummaryValues,
+    StationCalendarContext,
     TimingMetrics
   }
 
@@ -137,15 +90,14 @@ defmodule OrbitalDynamics.Communications.ContactContention do
       contact_types: @contact_types,
       contact_directions: @contact_directions,
       row_review_statuses: ["operator_review_required"],
-      station_unavailable_aliases: @unavailable_aliases,
-      station_availability_precedence: @station_availability_severity,
-      station_capacity_fraction_paths: @station_capacity_fraction_paths,
-      station_capacity_percent_paths: @station_capacity_percent_paths,
-      station_capacity_value_paths: capacity_value_path_metadata(@station_capacity_value_paths),
-      source_station_capacity_fraction_paths: @station_capacity_fraction_paths,
-      source_station_capacity_percent_paths: @station_capacity_percent_paths,
-      source_station_capacity_value_paths:
-        capacity_value_path_metadata(@station_capacity_value_paths),
+      station_unavailable_aliases: StationCalendarContext.unavailable_aliases(),
+      station_availability_precedence: StationCalendarContext.availability_precedence(),
+      station_capacity_fraction_paths: StationCalendarContext.capacity_fraction_paths(),
+      station_capacity_percent_paths: StationCalendarContext.capacity_percent_paths(),
+      station_capacity_value_paths: StationCalendarContext.capacity_value_path_metadata(),
+      source_station_capacity_fraction_paths: StationCalendarContext.capacity_fraction_paths(),
+      source_station_capacity_percent_paths: StationCalendarContext.capacity_percent_paths(),
+      source_station_capacity_value_paths: StationCalendarContext.capacity_value_path_metadata(),
       required_capacity_fraction_paths: CapacityDemand.required_capacity_fraction_paths(),
       required_capacity_percent_paths: CapacityDemand.required_capacity_percent_paths(),
       required_capacity_value_paths:
@@ -772,7 +724,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
     }
     |> Map.merge(contention_timing_metrics(contacts))
     |> Map.merge(contact_feedback_context(contacts))
-    |> Map.merge(station_calendar_context(contacts))
+    |> Map.merge(StationCalendarContext.build(contacts))
     |> compact_map()
   end
 
@@ -812,7 +764,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
     }
     |> Map.merge(contention_timing_metrics(contacts))
     |> Map.merge(contact_feedback_context(contacts))
-    |> Map.merge(station_calendar_context(contacts))
+    |> Map.merge(StationCalendarContext.build(contacts))
     |> compact_map()
   end
 
@@ -868,7 +820,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
     |> Map.merge(resolution_policy_context(policy))
     |> Map.merge(priority_field_evidence_context(contacts, policy))
     |> Map.merge(contact_feedback_context(contacts))
-    |> Map.merge(station_calendar_context(contacts))
+    |> Map.merge(StationCalendarContext.build(contacts))
     |> compact_map()
   end
 
@@ -907,7 +859,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
     |> Map.merge(resolution_policy_context(policy))
     |> Map.merge(priority_field_evidence_context(contacts, policy))
     |> Map.merge(contact_feedback_context(contacts))
-    |> Map.merge(station_calendar_context(contacts))
+    |> Map.merge(StationCalendarContext.build(contacts))
     |> compact_map()
   end
 
@@ -1009,271 +961,6 @@ defmodule OrbitalDynamics.Communications.ContactContention do
 
   defp contact_feedback_fields, do: FeedbackContext.fields()
 
-  defp station_calendar_context(contacts) do
-    %{
-      "station_availability" => station_availability(contacts),
-      "station_calendar_status" => station_calendar_status(contacts),
-      "capacity_fraction" => station_capacity_fraction(contacts),
-      "capacity_fraction_min" => station_capacity_fraction_min(contacts),
-      "capacity_fraction_max" => station_capacity_fraction_max(contacts),
-      "station_calendar_entry_ids" =>
-        stable_context_values(contacts, ["station_calendar_entry_id"]),
-      "station_calendar_provider_ids" =>
-        stable_context_values(contacts, ["station_calendar_provider_id"]),
-      "station_calendar_provider_entry_ids" =>
-        stable_context_values(contacts, ["station_calendar_provider_entry_id"]),
-      "station_calendar_overlap_entry_ids" =>
-        stable_context_values(contacts, ["station_calendar_overlap_entry_ids"]),
-      "station_calendar_directions" =>
-        string_context_values(contacts, ["station_calendar_directions"]),
-      "station_calendar_reservation_ids" =>
-        stable_context_values(contacts, ["station_calendar_reservation_ids"]),
-      "station_calendar_reserved_by" =>
-        string_context_values(contacts, ["station_calendar_reserved_by"]),
-      "station_calendar_reservation_statuses" =>
-        string_context_values(contacts, ["station_calendar_reservation_statuses"]),
-      "station_calendar_reservation_expires_at_s" =>
-        numeric_context_values(contacts, [
-          "station_calendar_reservation_expires_at_s",
-          "station_reservation_expires_at_s",
-          "reservation_expires_at_s"
-        ]),
-      "station_calendar_trust_boundary_statuses" =>
-        string_context_values(contacts, ["station_calendar_trust_boundary_status"]),
-      "station_reservation_ids" =>
-        stable_context_values(contacts, ["station_reservation_id", "reservation_id"]),
-      "station_reserved_bys" =>
-        string_context_values(contacts, ["station_reserved_by", "reserved_by"]),
-      "station_reservation_statuses" =>
-        string_context_values(contacts, ["station_reservation_status", "reservation_status"]),
-      "station_reservation_match_statuses" =>
-        string_context_values(contacts, [
-          "station_reservation_match_status",
-          "reservation_match_status"
-        ])
-    }
-    |> Enum.reject(fn {_key, value} -> value == [] end)
-    |> Map.new()
-  end
-
-  defp station_calendar_context_fields do
-    [
-      "station_availability",
-      "station_calendar_status",
-      "capacity_fraction",
-      "capacity_fraction_min",
-      "capacity_fraction_max",
-      "station_calendar_entry_ids",
-      "station_calendar_provider_ids",
-      "station_calendar_provider_entry_ids",
-      "station_calendar_overlap_entry_ids",
-      "station_calendar_directions",
-      "station_calendar_reservation_ids",
-      "station_calendar_reserved_by",
-      "station_calendar_reservation_statuses",
-      "station_calendar_reservation_expires_at_s",
-      "station_calendar_trust_boundary_statuses",
-      "station_reservation_ids",
-      "station_reserved_bys",
-      "station_reservation_statuses",
-      "station_reservation_match_statuses"
-    ]
-  end
-
-  defp station_availability(contacts) do
-    contacts
-    |> Enum.flat_map(&station_availability_candidates/1)
-    |> Enum.filter(&station_availability_value?/1)
-    |> highest_station_availability()
-    |> canonical_station_availability()
-  end
-
-  defp station_calendar_status(contacts) do
-    contacts
-    |> Enum.flat_map(&station_calendar_status_candidates/1)
-    |> Enum.filter(&station_availability_value?/1)
-    |> highest_station_availability()
-    |> canonical_station_availability()
-  end
-
-  defp station_capacity_fraction(contacts) do
-    contacts
-    |> station_capacity_fractions()
-    |> case do
-      [] -> nil
-      fractions -> Enum.min(fractions)
-    end
-  end
-
-  defp station_capacity_fraction_min(contacts), do: station_capacity_fraction(contacts)
-
-  defp station_capacity_fraction_max(contacts) do
-    contacts
-    |> station_capacity_fractions()
-    |> case do
-      [] -> nil
-      fractions -> Enum.max(fractions)
-    end
-  end
-
-  defp station_capacity_fractions(contacts) do
-    contacts
-    |> Enum.flat_map(&station_capacity_fraction_candidates/1)
-    |> Enum.map(&unit_interval_number/1)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.uniq()
-    |> Enum.sort()
-  end
-
-  defp station_capacity_fraction_candidates(contact) do
-    capacity_value_candidates(contact, @station_capacity_value_paths) ++
-      source_station_capacity_fraction_candidates(contact["source_station_calendar_entry"]) ++
-      source_station_capacity_fraction_candidates(contact["source_station_calendar_overlaps"])
-  end
-
-  defp source_station_capacity_fraction_candidates(sources) when is_list(sources),
-    do: Enum.flat_map(sources, &source_station_capacity_fraction_candidates/1)
-
-  defp source_station_capacity_fraction_candidates(%{} = source) do
-    capacity_value_candidates(source, @station_capacity_value_paths)
-  end
-
-  defp source_station_capacity_fraction_candidates(_source), do: []
-
-  defp capacity_value_candidates(value, paths) do
-    Enum.map(paths, fn
-      {:fraction, path} ->
-        path_value(value, path)
-
-      {:percent, path} ->
-        capacity_percent_fraction(path_value(value, path))
-    end)
-  end
-
-  defp path_value(value, [field]), do: Map.get(value, field)
-  defp path_value(value, path), do: get_in(value, path)
-
-  defp capacity_percent_fraction(value) do
-    case numeric_or_nil(value) do
-      value when is_number(value) and value >= 0.0 and value <= 100.0 -> value / 100.0
-      _value -> nil
-    end
-  end
-
-  defp unit_interval_number(value) do
-    case numeric_or_nil(value) do
-      value when is_number(value) and value >= 0.0 and value <= 1.0 -> value
-      _value -> nil
-    end
-  end
-
-  defp station_availability_candidates(contact) do
-    [
-      contact["station_availability"],
-      contact["availability"],
-      contact["station_calendar_status"],
-      contact["status"]
-    ] ++
-      source_station_calendar_availability_candidates(contact["source_station_calendar_entry"]) ++
-      source_station_calendar_availability_candidates(contact["source_station_calendar_overlaps"])
-  end
-
-  defp station_calendar_status_candidates(contact) do
-    [
-      contact["station_calendar_status"],
-      contact["status"]
-    ] ++
-      source_station_calendar_status_candidates(contact["source_station_calendar_entry"]) ++
-      source_station_calendar_status_candidates(contact["source_station_calendar_overlaps"])
-  end
-
-  defp source_station_calendar_availability_candidates(sources) when is_list(sources),
-    do: Enum.flat_map(sources, &source_station_calendar_availability_candidates/1)
-
-  defp source_station_calendar_availability_candidates(%{} = source) do
-    [
-      source["station_availability"],
-      source["availability"],
-      source["station_calendar_status"],
-      source["status"]
-    ]
-  end
-
-  defp source_station_calendar_availability_candidates(_source), do: []
-
-  defp source_station_calendar_status_candidates(sources) when is_list(sources),
-    do: Enum.flat_map(sources, &source_station_calendar_status_candidates/1)
-
-  defp source_station_calendar_status_candidates(%{} = source) do
-    [
-      source["station_calendar_status"],
-      source["status"],
-      source["availability"]
-    ]
-  end
-
-  defp source_station_calendar_status_candidates(_source), do: []
-
-  defp highest_station_availability([]), do: nil
-
-  defp highest_station_availability(values),
-    do: Enum.max_by(values, &station_availability_severity/1)
-
-  defp station_availability_value?(value)
-       when value in ["available", "unavailable", "maintenance", "reserved", "reduced_capacity"],
-       do: true
-
-  defp station_availability_value?(value) when value in @unavailable_aliases, do: true
-  defp station_availability_value?(_value), do: false
-
-  defp station_availability_severity(value) when value in @unavailable_aliases,
-    do: @station_availability_severity["unavailable"]
-
-  defp station_availability_severity(value), do: Map.get(@station_availability_severity, value, 0)
-
-  defp canonical_station_availability(value)
-       when value in ["unavailable", "maintenance" | @unavailable_aliases],
-       do: "unavailable"
-
-  defp canonical_station_availability(value), do: value
-
-  defp stable_context_values(contacts, fields) do
-    contacts
-    |> context_values(fields)
-    |> Enum.map(&stable_id_or_nil/1)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.uniq()
-    |> Enum.sort()
-  end
-
-  defp string_context_values(contacts, fields) do
-    contacts
-    |> context_values(fields)
-    |> Enum.map(&encode_value/1)
-    |> Enum.filter(&(is_binary(&1) and &1 != ""))
-    |> Enum.uniq()
-    |> Enum.sort()
-  end
-
-  defp numeric_context_values(contacts, fields) do
-    contacts
-    |> context_values(fields)
-    |> Enum.map(&numeric_or_nil/1)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.uniq()
-    |> Enum.sort()
-  end
-
-  defp context_values(contacts, fields) do
-    Enum.flat_map(contacts, fn contact ->
-      Enum.flat_map(fields, fn field ->
-        contact
-        |> Map.get(field)
-        |> List.wrap()
-      end)
-    end)
-  end
-
   defp maybe_apply_group_approval_policy(group, nil), do: group
 
   defp maybe_apply_group_approval_policy(group, approval_policy) do
@@ -1364,7 +1051,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
           "scenario_ids" => group["scenario_ids"]
         }
         |> Map.merge(Map.take(group, contact_feedback_fields()))
-        |> Map.merge(Map.take(group, station_calendar_context_fields()))
+        |> Map.merge(Map.take(group, StationCalendarContext.fields()))
         |> compact_map()
     }
     |> compact_map()
@@ -1448,7 +1135,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
           "policy_warnings" => recommendation["policy_warnings"]
         }
         |> Map.merge(Map.take(recommendation, contact_feedback_fields()))
-        |> Map.merge(Map.take(recommendation, station_calendar_context_fields()))
+        |> Map.merge(Map.take(recommendation, StationCalendarContext.fields()))
         |> compact_map()
     }
     |> compact_map()
@@ -1951,7 +1638,7 @@ defmodule OrbitalDynamics.Communications.ContactContention do
   defp normalize_contact(contact) do
     ContactNormalization.normalize(
       contact,
-      @unavailable_aliases,
+      StationCalendarContext.unavailable_aliases(),
       @default_resolution_priority_fields,
       @provider_direction_aliases
     )
