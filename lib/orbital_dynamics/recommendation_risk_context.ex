2f5819232831586f203635128d13459932b4bc92
Donovan Rost <donovan.rost@gmail.com>
2026-07-19 17:05:36 -0600

    refactor objective tradeoff risk context

--- lib/orbital_dynamics/recommendation_risk_context.ex
+++ lib/orbital_dynamics/recommendation_risk_context.ex
@@ -6,6 +6,7 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
     ContactIntent,
     ExecutionSuccessFeedback,
     ObjectiveSatisfaction,
+    ObjectiveTradeoff,
     OperationalFeedback,
     ResourceProjection,
     StationCalendar,
@@ -451,43 +452,6 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
     "score_term_pressure_derivation_reasons"
   ]
 
-  @objective_tradeoff_context_keys [
-    "objective_tradeoff_pressure_risk_types",
-    "objective_tradeoff_pressure_objective_ids",
-    "objective_tradeoff_pressure_objective_types",
-    "objective_tradeoff_pressure_latency_objective_values",
-    "objective_tradeoff_pressure_target_ids",
-    "objective_tradeoff_pressure_scenario_ids",
-    "objective_tradeoff_pressure_branch_ids",
-    "objective_tradeoff_pressure_ground_station_ids",
-    "objective_tradeoff_pressure_collection_ids",
-    "objective_tradeoff_pressure_product_ids",
-    "objective_tradeoff_pressure_payload_ids",
-    "objective_tradeoff_pressure_instrument_ids",
-    "objective_tradeoff_pressure_start_values_s",
-    "objective_tradeoff_pressure_end_values_s",
-    "objective_tradeoff_pressure_required_contact_values",
-    "objective_tradeoff_pressure_planned_contact_values",
-    "objective_tradeoff_pressure_required_downlink_values_mb",
-    "objective_tradeoff_pressure_planned_downlink_values_mb",
-    "objective_tradeoff_pressure_max_latency_values_s",
-    "objective_tradeoff_pressure_planned_latency_values_s",
-    "objective_tradeoff_pressure_required_observation_values",
-    "objective_tradeoff_pressure_planned_observation_values",
-    "objective_tradeoff_pressure_priorities",
-    "objective_tradeoff_pressure_latitude_values_deg",
-    "objective_tradeoff_pressure_longitude_values_deg",
-    "objective_tradeoff_pressure_minimum_elevation_values_deg",
-    "objective_tradeoff_pressure_source_activity_ids",
-    "objective_tradeoff_pressure_score_values",
-    "objective_tradeoff_pressure_score_delta_from_selected_values",
-    "objective_tradeoff_pressure_score_term_maps",
-    "objective_tradeoff_pressure_feedback_sources",
-    "objective_tradeoff_pressure_feedback_scopes",
-    "objective_tradeoff_pressure_trust_boundaries",
-    "objective_tradeoff_pressure_derivation_reasons"
-  ]
-
   @resource_margin_context_keys [
     "resource_margin_risk_types",
     "resource_margin_spacecraft_ids",
@@ -618,7 +582,7 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
 
   def objective_satisfaction_context_keys, do: ObjectiveSatisfaction.context_keys()
 
-  def objective_tradeoff_context_keys, do: @objective_tradeoff_context_keys
+  def objective_tradeoff_context_keys, do: ObjectiveTradeoff.context_keys()
 
   def resource_margin_context_keys, do: @resource_margin_context_keys
 
@@ -1602,90 +1566,7 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
 
   def objective_satisfaction_context(risks), do: ObjectiveSatisfaction.context(risks)
 
-  def objective_tradeoff_context(risks) when is_list(risks) do
-    risks = Enum.map(risks, &stringify_keys/1)
-
-    objective_tradeoff_risks =
-      Enum.filter(risks, &objective_tradeoff_risk?/1)
-
-    %{
-      "objective_tradeoff_pressure_risk_types" =>
-        risk_context_values(objective_tradeoff_risks, ["type", "risk_type"]),
-      "objective_tradeoff_pressure_objective_ids" =>
-        risk_context_values(objective_tradeoff_risks, "objective_id"),
-      "objective_tradeoff_pressure_objective_types" =>
-        risk_context_values(objective_tradeoff_risks, "objective_type"),
-      "objective_tradeoff_pressure_latency_objective_values" =>
-        risk_context_values(objective_tradeoff_risks, "latency_objective"),
-      "objective_tradeoff_pressure_target_ids" =>
-        risk_context_values(objective_tradeoff_risks, "target_id"),
-      "objective_tradeoff_pressure_scenario_ids" =>
-        risk_context_values(objective_tradeoff_risks, "scenario_id"),
-      "objective_tradeoff_pressure_branch_ids" =>
-        risk_context_values(objective_tradeoff_risks, "branch_id"),
-      "objective_tradeoff_pressure_ground_station_ids" =>
-        risk_context_values(objective_tradeoff_risks, "ground_station_id"),
-      "objective_tradeoff_pressure_collection_ids" =>
-        risk_context_values(objective_tradeoff_risks, ["collection_id", "collection_ids"]),
-      "objective_tradeoff_pressure_product_ids" =>
-        risk_context_values(objective_tradeoff_risks, ["product_id", "product_ids"]),
-      "objective_tradeoff_pressure_payload_ids" =>
-        risk_context_values(objective_tradeoff_risks, ["payload_id", "payload_ids"]),
-      "objective_tradeoff_pressure_instrument_ids" =>
-        risk_context_values(objective_tradeoff_risks, ["instrument_id", "instrument_ids"]),
-      "objective_tradeoff_pressure_start_values_s" =>
-        risk_context_values(objective_tradeoff_risks, "starts_at_s"),
-      "objective_tradeoff_pressure_end_values_s" =>
-        risk_context_values(objective_tradeoff_risks, "ends_at_s"),
-      "objective_tradeoff_pressure_required_contact_values" =>
-        risk_context_values(objective_tradeoff_risks, "required_contacts"),
-      "objective_tradeoff_pressure_planned_contact_values" =>
-        risk_context_values(objective_tradeoff_risks, "planned_contacts"),
-      "objective_tradeoff_pressure_required_downlink_values_mb" =>
-        risk_context_values(objective_tradeoff_risks, "required_downlink_mb"),
-      "objective_tradeoff_pressure_planned_downlink_values_mb" =>
-        risk_context_values(objective_tradeoff_risks, "planned_downlink_mb"),
-      "objective_tradeoff_pressure_max_latency_values_s" =>
-        risk_context_values(objective_tradeoff_risks, "max_latency_s"),
-      "objective_tradeoff_pressure_planned_latency_values_s" =>
-        risk_context_values(objective_tradeoff_risks, "planned_latency_s"),
-      "objective_tradeoff_pressure_required_observation_values" =>
-        risk_context_values(objective_tradeoff_risks, "required_observations"),
-      "objective_tradeoff_pressure_planned_observation_values" =>
-        risk_context_values(objective_tradeoff_risks, "planned_observations"),
-      "objective_tradeoff_pressure_priorities" =>
-        risk_context_values(objective_tradeoff_risks, "priority"),
-      "objective_tradeoff_pressure_latitude_values_deg" =>
-        risk_context_values(objective_tradeoff_risks, "latitude_deg"),
-      "objective_tradeoff_pressure_longitude_values_deg" =>
-        risk_context_values(objective_tradeoff_risks, "longitude_deg"),
-      "objective_tradeoff_pressure_minimum_elevation_values_deg" =>
-        risk_context_values(objective_tradeoff_risks, "minimum_elevation_deg"),
-      "objective_tradeoff_pressure_source_activity_ids" =>
-        risk_context_values(objective_tradeoff_risks, [
-          "source_activity_id",
-          "source_activity_ids"
-        ]),
-      "objective_tradeoff_pressure_score_values" =>
-        risk_context_values(objective_tradeoff_risks, "score"),
-      "objective_tradeoff_pressure_score_delta_from_selected_values" =>
-        risk_context_values(objective_tradeoff_risks, "score_delta_from_selected"),
-      "objective_tradeoff_pressure_score_term_maps" =>
-        risk_context_values(objective_tradeoff_risks, "score_terms"),
-      "objective_tradeoff_pressure_feedback_sources" =>
-        risk_context_values(objective_tradeoff_risks, "feedback_source"),
-      "objective_tradeoff_pressure_feedback_scopes" =>
-        risk_context_values(objective_tradeoff_risks, "feedback_scope"),
-      "objective_tradeoff_pressure_trust_boundaries" =>
-        risk_context_values(objective_tradeoff_risks, "trust_boundary"),
-      "objective_tradeoff_pressure_derivation_reasons" =>
-        risk_context_values(objective_tradeoff_risks, ["derivation_reasons"])
-    }
-    |> Enum.reject(fn {_key, values} -> values == [] end)
-    |> Map.new()
-  end
-
-  def objective_tradeoff_context(_risks), do: %{}
+  def objective_tradeoff_context(risks), do: ObjectiveTradeoff.context(risks)
 
   def resource_margin_context(risks) when is_list(risks) do
     risks = Enum.map(risks, &stringify_keys/1)
@@ -1980,10 +1861,6 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
   defp score_term_risk?(%{"feedback_scope" => "score_term"}), do: true
 
   defp score_term_risk?(_risk), do: false
-
-  defp objective_tradeoff_risk?(%{"feedback_scope" => "objective_tradeoff"}), do: true
-
-  defp objective_tradeoff_risk?(_risk), do: false
 
   defp risk_context_values(risks, keys) when is_list(keys) do
     risks
