7054c53bb0e381c8f6e97fabf70f375fe49aba9b
Donovan Rost <donovan.rost@gmail.com>
2026-07-19 21:37:03 -0600

    Extract timeline preservation context

--- lib/orbital_dynamics/recommendation_risk_context.ex
+++ lib/orbital_dynamics/recommendation_risk_context.ex
@@ -27,38 +27,10 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
     TimelineDependencyImpact,
     TimelineIntegrity,
     TimelineLifecycleState,
+    TimelinePreservation,
     TimelinePublication,
     ValidationRefresh
   }
-
-  @timeline_preservation_context_keys [
-    "timeline_preservation_activity_ids",
-    "timeline_preservation_timeline_ids",
-    "timeline_preservation_statuses",
-    "timeline_preservation_requires_preservation_values",
-    "timeline_preservation_requires_operator_review_values",
-    "timeline_preservation_protection_decisions",
-    "timeline_preservation_protection_categories",
-    "timeline_preservation_protection_reasons",
-    "timeline_preservation_preserve_activity_count_values",
-    "timeline_preservation_review_change_activity_count_values",
-    "timeline_preservation_sensitive_activity_count_values",
-    "timeline_preservation_preserve_activity_ids",
-    "timeline_preservation_preserve_timeline_ids",
-    "timeline_preservation_review_change_activity_ids",
-    "timeline_preservation_review_change_timeline_ids",
-    "timeline_preservation_sensitive_activity_ids",
-    "timeline_preservation_sensitive_timeline_ids",
-    "timeline_preservation_invalid_activity_input_values",
-    "timeline_preservation_invalid_activity_input_reasons",
-    "timeline_preservation_required_operator_actions",
-    "timeline_preservation_feedback_sources",
-    "timeline_preservation_feedback_scopes",
-    "timeline_preservation_feedback_keys",
-    "timeline_preservation_trust_boundaries",
-    "timeline_preservation_derivation_reasons",
-    "timeline_preservation_assumption_maps"
-  ]
 
   @timeline_publication_context_keys [
     "timeline_publication_ids",
@@ -212,7 +184,7 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
   def timeline_activity_precondition_context_keys,
     do: TimelineActivityPrecondition.context_keys()
 
-  def timeline_preservation_context_keys, do: @timeline_preservation_context_keys
+  def timeline_preservation_context_keys, do: TimelinePreservation.context_keys()
 
   def timeline_publication_context_keys, do: @timeline_publication_context_keys
 
@@ -285,79 +257,7 @@ defmodule OrbitalDynamics.RecommendationRiskContext do
   def timeline_activity_precondition_context(risks),
     do: TimelineActivityPrecondition.context(risks)
 
-  def timeline_preservation_context(risks) when is_list(risks) do
-    risks = Enum.map(risks, &stringify_keys/1)
-
-    timeline_preservation_risks =
-      Enum.filter(
-        risks,
-        &(Map.get(&1, "type") == "timeline_preservation_review" or
-            Map.get(&1, "feedback_scope") == "timeline_preservation")
-      )
-
-    %{
-      "timeline_preservation_activity_ids" =>
-        risk_context_values(timeline_preservation_risks, "activity_id"),
-      "timeline_preservation_timeline_ids" =>
-        risk_context_values(timeline_preservation_risks, "timeline_id"),
-      "timeline_preservation_statuses" =>
-        risk_context_values(timeline_preservation_risks, "timeline_preservation_status"),
-      "timeline_preservation_requires_preservation_values" =>
-        risk_context_values(timeline_preservation_risks, "requires_preservation"),
-      "timeline_preservation_requires_operator_review_values" =>
-        risk_context_values(timeline_preservation_risks, "requires_operator_review"),
-      "timeline_preservation_protection_decisions" =>
-        risk_context_values(timeline_preservation_risks, "protection_decision"),
-      "timeline_preservation_protection_categories" =>
-        risk_context_values(timeline_preservation_risks, "protection_category"),
-      "timeline_preservation_protection_reasons" =>
-        risk_context_values(timeline_preservation_risks, "protection_reason"),
-      "timeline_preservation_preserve_activity_count_values" =>
-        risk_context_values(timeline_preservation_risks, "preserve_activity_count"),
-      "timeline_preservation_review_change_activity_count_values" =>
-        risk_context_values(timeline_preservation_risks, "review_change_activity_count"),
-      "timeline_preservation_sensitive_activity_count_values" =>
-        risk_context_values(timeline_preservation_risks, "preservation_sensitive_activity_count"),
-      "timeline_preservation_preserve_activity_ids" =>
-        risk_context_values(timeline_preservation_risks, ["preserve_activity_ids"]),
-      "timeline_preservation_preserve_timeline_ids" =>
-        risk_context_values(timeline_preservation_risks, ["preserve_timeline_ids"]),
-      "timeline_preservation_review_change_activity_ids" =>
-        risk_context_values(timeline_preservation_risks, ["review_change_activity_ids"]),
-      "timeline_preservation_review_change_timeline_ids" =>
-        risk_context_values(timeline_preservation_risks, ["review_change_timeline_ids"]),
-      "timeline_preservation_sensitive_activity_ids" =>
-        risk_context_values(timeline_preservation_risks, [
-          "preservation_sensitive_activity_ids"
-        ]),
-      "timeline_preservation_sensitive_timeline_ids" =>
-        risk_context_values(timeline_preservation_risks, [
-          "preservation_sensitive_timeline_ids"
-        ]),
-      "timeline_preservation_invalid_activity_input_values" =>
-        risk_context_values(timeline_preservation_risks, "invalid_activity_input"),
-      "timeline_preservation_invalid_activity_input_reasons" =>
-        risk_context_values(timeline_preservation_risks, "invalid_activity_input_reason"),
-      "timeline_preservation_required_operator_actions" =>
-        risk_context_values(timeline_preservation_risks, "required_operator_action"),
-      "timeline_preservation_feedback_sources" =>
-        risk_context_values(timeline_preservation_risks, "feedback_source"),
-      "timeline_preservation_feedback_scopes" =>
-        risk_context_values(timeline_preservation_risks, "feedback_scope"),
-      "timeline_preservation_feedback_keys" =>
-        risk_context_values(timeline_preservation_risks, "feedback_key"),
-      "timeline_preservation_trust_boundaries" =>
-        risk_context_values(timeline_preservation_risks, "trust_boundary"),
-      "timeline_preservation_derivation_reasons" =>
-        risk_context_values(timeline_preservation_risks, ["derivation_reasons"]),
-      "timeline_preservation_assumption_maps" =>
-        risk_context_values(timeline_preservation_risks, "assumptions")
-    }
-    |> Enum.reject(fn {_key, values} -> values == [] end)
-    |> Map.new()
-  end
-
-  def timeline_preservation_context(_risks), do: %{}
+  def timeline_preservation_context(risks), do: TimelinePreservation.context(risks)
 
   def timeline_publication_context(risks), do: TimelinePublication.context(risks)
 
